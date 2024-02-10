ebb506170c6c68506ddd85c4abf88a6afa4ee37b
codeico <bangico2002@gmail.com>
2024-02-10 07:02:49 +0700

    Update route.ts

--- app/api/frame/route.ts
+++ app/api/frame/route.ts
@@ -34,8 +34,9 @@ async function getResponse(req: NextRequest): Promise<NextResponse> {
   <title>This is frame ${id}</title>
   <meta property="fc:frame" content="vNext" />
   <meta property="fc:frame:image" content="https://codeico.id/nft/${id}.png" />
-  <meta property="fc:frame:button:1" content="CLAIM!!" />
+  <meta property="fc:frame:button:1" content="SUBMIT WALLET" />
   <meta property="fc:frame:post_url" content="https://bangcode.vercel.app/api/frame?id=${nextId}" />
+  <meta property="fc:frame:input:text" content="0x...." />
 </head></html>`);
 } else if(idAsNumber === 6){
   return new NextResponse(`<!DOCTYPE html><html><head>
@@ -52,6 +53,8 @@ async function getResponse(req: NextRequest): Promise<NextResponse> {
     <meta property="fc:frame:image" content="https://codeico.id/nft/${id}.png" />
     <meta property="fc:frame:button:1" content="DONE!!" />
     <meta property="fc:frame:post_url" content="https://bangcode.vercel.app/api/frame?id=${nextId}" />
+    <meta property="fc:frame:button:2" content="Recast!!" />
+    <meta property="fc:frame:post_url" content="https://warpcast.com/~/compose?text=LFG%20$BCD!&embeds[]=https://bangcode.vercel.app" />
   </head></html>`);
   }
 }
